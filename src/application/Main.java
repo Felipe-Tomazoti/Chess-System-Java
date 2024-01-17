9bcfc77ffa130cacf9b9ad50fafaffc41018a75b
Felipe-Tomazoti <felipecesar005@gmail.com>
2024-01-16 21:39:09 -0300

    Special move - Promotion

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,7 @@ import boardgame.Board;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import javax.print.DocFlavor;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -38,6 +39,11 @@ public class Main {
 
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
+                }
+                if(chessMatch.getPromoted() != null){
+                    System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
                 }
             } catch(ChessException e){
                 System.out.println(e.getMessage());
